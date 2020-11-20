0ca3d20dda2d92455f77ac8b030df8a7ed04b758
Casey Hancock <casey@hancock.tech>
2020-11-19 23:51:30 -0500

    fix: move handler into handlers pkg

--- handlers/proxy.go
+++ handlers/proxy.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"cloud.google.com/go/storage"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/sirupsen/logrus"
@@ -103,4 +105,28 @@ func (h *proxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func Proxy(c Config, hc *http.Client) http.Handler {
 	logger := c.Logger()
 	return &proxyHandler{logger: logger, hc: hc, config: c}
+}
+
+func Handler(c Config, client *storage.Client, hc *http.Client) http.HandlerFunc {
+	proxyHandler := Proxy(c, hc)
+	mapHandler := Map(c, client)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case strings.HasPrefix(r.URL.Path, c.Proxy.Endpoint):
+			r.URL.Path = strings.Replace(r.URL.Path, c.Proxy.Endpoint, "", 1)
+			if !strings.HasPrefix(r.URL.Path, "/") {
+				r.URL.Path = "/" + r.URL.Path
+			}
+			proxyHandler.ServeHTTP(w, r)
+		case strings.HasPrefix(r.URL.Path, c.Map.Endpoint):
+			r.URL.Path = strings.Replace(r.URL.Path, c.Map.Endpoint, "", 1)
+			mapHandler.ServeHTTP(w, r)
+		case r.URL.Path == "/":
+			// healthcheck
+			w.WriteHeader(http.StatusOK)
+		default:
+			http.Error(w, "not found", http.StatusNotFound)
+		}
+	}
 }
